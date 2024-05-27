9997d83897fb9d5657e744dae6b3b3eeaaf5f788
Chencc <599303650@qq.com>
2024-05-27 09:08:46 +0800

    1

---
+++ single-responesibility-principle/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author unknown  
+ * @since 2024/05/24 10:37
+ */
+  public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
